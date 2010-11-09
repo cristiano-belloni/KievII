59ccfba630b79222d713465c87b9e2a8a6742bbf
Cristiano Belloni <daniel_mendoza@libero.it>
2010-11-09 13:55:22 +0100

    "Aesthetic" jsLint-conformancy on Element.js

--- graphic_elements/Element.js
+++ graphic_elements/Element.js
@@ -1,5 +1,7 @@
 function Element(name, topleft) {
-  if( arguments.length ) { this.getready(name, topleft); }
+    if (arguments.length) {
+        this.getready(name, topleft);
+    }
 }
 
 Element.prototype.getready = function (name, topleft) {
@@ -15,110 +17,110 @@ Element.prototype.getready = function (name, topleft) {
     // Element never draws itself.
     this.drawItself = false;
     // These are to be set later
-    this.values = new Object();
+    this.values = {};
     // Completion
     this.objectsTotal = 0;
     this.objectsLoaded = 0;
     this.completed = false;
 };
 
-Element.prototype.IsInROI  = function(x, y) {
+Element.prototype.IsInROI = function (x, y) {
     // This is the abstract class.
     return false;
-}
+};
 
 Element.prototype.onROI = function (start_x, start_y, curr_x, curr_y) {
     // This is the abstract class.
     var ret = {"slot" : undefined, "value" : undefined};
     return ret;
-}
+};
 
-Element.prototype.getValues = function() {
+Element.prototype.getValues = function () {
     var tempArray = [];
     for (var i in this.values) {
         tempArray.push(i);
     }
     // Returns the keys.
     return tempArray;
-}
+};
 
-Element.prototype.getXCoord = function() {
-        return this.xOrigin;
-}
+Element.prototype.getXCoord = function () {
+    return this.xOrigin;
+};
 
-Element.prototype.getYCoord = function() {
-        return this.yOrigin;
-}
+Element.prototype.getYCoord = function () {
+    return this.yOrigin;
+};
 
-Element.prototype.getWidth = function() {
-        return this.width;
-}
+Element.prototype.getWidth = function () {
+    return this.width;
+};
 
-Element.prototype.getHeight = function() {
-        return this.height;
-}
+Element.prototype.getHeight = function () {
+    return this.height;
+};
 
-Element.prototype.getValue = function(slot) {
-    if (this.values[slot] == undefined) {
-        throw new Error ("Slot " + slot + " not present or value undefined");
+Element.prototype.getValue = function (slot) {
+    if (this.values[slot] === undefined) {
+        throw new Error("Slot " + slot + " not present or value undefined");
     }
     else {
         return this.values[slot];
     }
-}
+};
 
-Element.prototype.getStatus = function() {
+Element.prototype.getStatus = function () {
     return {"objectsTotal" : this.objectsTotal, "objectsLoaded" : this.objectsLoaded};
-}
+};
 
-Element.prototype.isComplete = function() {
+Element.prototype.isComplete = function () {
     return this.completed;
-}
+};
 
-Element.prototype.onCompletion = function() {
+Element.prototype.onCompletion = function () {
     return;
-}
+};
 
 // Setters
-Element.prototype.setValue = function(slot, value) {
+Element.prototype.setValue = function (slot, value) {
 
     var temp_value = value;
 
-    if (this.values[slot] == undefined) {
-        throw new Error ("Slot " + slot + " not present or value undefined");
+    if (this.values[slot] === undefined) {
+        throw new Error("Slot " + slot + " not present or value undefined");
     }
 
-    if (temp_value == this.values[slot]) {
+    if (temp_value === this.values[slot]) {
         // Nothing to do.
         return;
     }
 
     this.values[slot] = temp_value;
 
-    if (this.drawItself == true) {
+    if (this.drawItself === true) {
         this.refresh();
     }
 
-}
+};
 
 Element.prototype.setClickable = function (isClickable) {
     this.isClickable = isClickable;
-}
+};
 
-Element.prototype.setDrawsItself = function(value) {
+Element.prototype.setDrawsItself = function (value) {
     this.drawItself = value;
-}
+};
 
 // Refresh. This is the basic action.
-Element.prototype.refresh = function() {
+Element.prototype.refresh = function () {
     this.drawFunc();
-}
+};
 
-Element.prototype.getName = function() {
-        return this.name;
-}
+Element.prototype.getName = function () {
+    return this.name;
+};
 
 Element.prototype.setDrawClass = function (drawClass) {
     this.drawClass = drawClass;
-}
+};
 
