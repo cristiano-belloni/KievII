1e1bd3cd5cfba335c8a16e53335a2690f47edd43
Cristiano Belloni <daniel_mendoza@libero.it>
2010-11-17 01:55:50 +0100

    Pitchshifter is initialized on init() (when channel's samplerate is available) +Fixed a stupid class name error.

--- audio/ADShifterFilter.js
+++ audio/ADShifterFilter.js
@@ -14,9 +14,8 @@ function AudioDataShifterFilter(next, shifterParams) {
    * @type object
    */
   this.__shifterParams = shifterParams;
-  this.__shifter = new Pitchshift(this.__shifterParams.fftFrameSize, this.audioParameters.sampleRate);
   this.shiftAmount = (this.__shifterParams.shiftAmount); // Initial shift amount
-  this.osamp = (this.__shifterParams.osamp);             // Initial osamp amount (?)
+  this.osamp = (this.__shifterParams.osamp);             // Initial osamp amount 
 
 }
 
@@ -28,7 +27,10 @@ AudioDataShifterFilter.prototype = new AudioDataFilter(null);
  */
 AudioDataShifterFilter.prototype.init = function (audioParameters) {
   AudioDataFilter.prototype.init.call(this, audioParameters);
-  this.__updateCoefficients();
+  if (this.__shifter == undefined) {
+      console.log ("Lazily initializing the pitchshifter");
+      this.__shifter = new Pitchshift(this.__shifterParams.fftFrameSize, audioParameters.sampleRate);
+  }
 };
 
 /**
@@ -36,14 +38,21 @@ AudioDataShifterFilter.prototype.init = function (audioParameters) {
  * @param {Array} data The signal data.
  * @param {int} length The signal data to be processed starting from the beginning.
  */
-AudioDataLowPassFilter.prototype.process = function (data, length) {
-  if (length === 0) {
-    return;
-  }
+AudioDataShifterFilter.prototype.process = function (data, length) {
+    console.log ("We got some shit to pass to process; seem " + length + " samples, pitchShift factor is " +  this.shiftAmount + " und indata.length is " + data.length);
+    if (length === 0) {
+        return;
+    }
 
-  //var channels = this.audioParameters.channels;
+    //var channels = this.audioParameters.channels;
+    this.__shifter.process (this.shiftAmount, length, this.osamp, data);
+    console.log ("After processing");
 
-  this.__shifter.process (this.shiftAmount, length, this.osamp, data)
+    console.log ("After processing, we got an outData that is long " + this.__shifter.outdata);
+    //console.log ("o[0] " + this.__shifter.outdata[0] + " o[1] " + this.__shifter.outdata[1] + " o[2] " + this.__shifter.outdata[2] + " o[3] " + this.__shifter.outdata[3] + " o[4] " + this.__shifter.outdata[4] + "o[5] " + this.__shifter.outdata[5] + "o[6] " + this.__shifter.outdata[6] + "o[7] " + this.__shifter.outdata[7] + "o[8] " + this.__shifter.outdata[8] + "o[9] " + this.__shifter.outdata[9] + "o[10] " + this.__shifter.outdata[10]);
+    data = this.__shifter.outdata;
+    console.log ("data that is long " + this.__shifter.outdata);
+    //console.log ("o[0] " + data[0] + " o[1] " + data[1] + " o[2] " + data[2] + " o[3] " + data[3] + " o[4] " + data[4] + "o[5] " + data[5] + "o[6] " + data[6] + "o[7] " + data[7] + "o[8] " + data[8] + "o[9] " + data[9] + "o[10] " + data[10]);
 
 };
 
