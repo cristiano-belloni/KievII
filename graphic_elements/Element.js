5ca4f29272097e5b8dc5c3fd0be5ce4dc583977f
Janesconference <janesconference@gmail.com>
2011-02-09 12:17:20 +0100

    Added graphic element name to be returned in onValueSet callback.

--- graphic_elements/Element.js
+++ graphic_elements/Element.js
@@ -129,7 +129,7 @@ Element.prototype.setValue = function (slot, value) {
 
     // Finally, call the callback if there's one.
     if (typeof (this.onValueSet) === "function") {
-        this.onValueSet (slot, this.values[slot]);
+        this.onValueSet (slot, this.values[slot], this.name);
     }
 
 };
