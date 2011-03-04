78eb1b0c8eb3692bdb132745b719729b3a30f717
Janesconference <janesconference@gmail.com>
2011-03-04 12:41:54 +0100

    Modified the upload script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 . local.mk
 rm git_info.log 2>/dev/null
-echo `date` > git_info.log
-echo '*****************************' >> git_info.log
-git ls-files | while read file; do git log -n 1 --pretty="$file, commit: %h, date: %ad" -- $file >> git_info.log; done
+git log -n 1 --pretty="%H">git_info.log
 scp -r git_info.log audio/ demos/ graphic_elements/ dsp/ $remotehost:$remotedir
