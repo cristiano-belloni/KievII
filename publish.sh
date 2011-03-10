662ce563248ac8f3bf8f55e95f893ab12f9ef9fb
Janesconference <janesconference@gmail.com>
2011-03-10 01:07:53 +0100

    Fixed some deploying problems.

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 . local.mk
 rm git_info.log 2>/dev/null
 git log -n 1 --pretty="%H">git_info.log
-scp -r git_info.log audio/ demos/ graphic_elements/ dsp/ $remotehost:$remotedir
+scp -r git_info.log utilities/ audio/ demos/ graphic_elements/ dsp/ $remotehost:$remotedir
