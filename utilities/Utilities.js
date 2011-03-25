ed3db9a055c2fdc3a5d8f7993dcd7f03ddbaf0c4
Janesconference <janesconference@gmail.com>
2011-03-25 13:23:49 +0100

    Added a loadMultipleImages class.

--- utilities/Utilities.js
+++ utilities/Utilities.js
@@ -74,3 +74,65 @@ function loadImageArray (args) {
         this.imagesArray[i].src = args.imageNames[i];
     }
 }
+
+function loadMultipleImages (args) {
+
+    this.loadingManager = function () {
+        // Storage the closurage.
+        var that = this;
+        return function (loaderStatus) {
+            var ls = loaderStatus;
+                    console.log (ls.status.id, " called back to say everything is loaded.");
+
+                    // Update the element status
+                    if (that.loaders[ls.status.id] !== undefined) {
+                        that.loaders[ls.status.id].done = true;
+                        that.loaders[ls.status.id].images = that.loaders[ls.status.id].imageArray.imagesArray;
+                    }
+                    else {
+                        throw new Error("in loaders, " + ls.status.id + " is undefined");
+                    }
+
+                    // Check if every registered element is complete.
+                    for (var element in that.loaders) {
+                        if (that.loaders.hasOwnProperty(element)) {
+                            if (that.loaders[element].done !== true) {
+                                console.log ("status of element ", element, " is not true: ", that.loaders[element].done);
+                                return;
+                            }
+                        }
+                    }
+
+                    that.onComplete (that.loaders);
+                }
+            }
+
+    // Error & single; to be done
+
+
+    /*{ID : "ztest_button_image_loader",
+          imageNames: ["button1_1.png", "button1_2.png"],
+          onComplete: loadingManager() }*/
+    // {multipleImages: [{imageNames: [], id: ""}, {..}], onComplete: func, onError: func, onSingle: func};
+    //throw Error ("multipleImages is not defined");
+    this.multipleImages = args.multipleImages;
+    this.onComplete = args.onComplete;
+    this.onError = args.onError;
+    this.onSingle = args.onSingle;
+    this.loaders = {};
+
+    //init the shit
+    for (var i = 0; i < this.multipleImages.length; i += 1) {
+
+        var loader = {};
+        loader.imageArray = new loadImageArray ({ID : this.multipleImages[i].ID,
+                                                 imageNames: this.multipleImages[i].imageNames,
+                                                 onComplete: this.loadingManager()
+                                                });
+        loader.done = false;
+        this.loaders[this.multipleImages[i].ID] = loader;
+    }
+
+
+
+}
