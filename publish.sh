1bf484674dfd27c82e0655163ce9940f7f22c9b5
Cristiano Belloni <janesconference@gmail.com>
2012-11-20 16:01:22 +0100

    Fixed a bug in the publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm version.js 2>/dev/null
-git commit -a && echo K2.version=\"`git log -n 1 --pretty="%H"`\"\; && grunt release
+git commit -a && echo K2.version=\"`git log -n 1 --pretty="%H"`\"\; > version.js && grunt release
