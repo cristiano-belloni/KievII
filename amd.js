f67b8a0de63b1405e303d8934c7d1cab9901bad5
Cristiano Belloni <janesconference@gmail.com>
2013-02-12 12:51:57 +0100

    Added debug info for AMD detection

--- amd.js
+++ amd.js
@@ -1,8 +1,10 @@
 if (typeof window.define === "function" && window.define.amd) {
   window.define("kievII", [], function() {
+    console.log ("KievII: returning K2 object inside the define (AMD detected)");
     return K2;
   });
 }
 else {
+    console.log ("KievII: setting window.K2 (no AMD)");
     window.kievII = window.K2 = K2;
 }
