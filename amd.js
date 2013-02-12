d9e19d6d257b6b3f06cf374d32edff857b60e628
Cristiano Belloni <janesconference@gmail.com>
2013-02-12 12:26:01 +0100

    Updated AMD support (won't set up window.K2 object if AMD-loading)

--- amd.js
+++ amd.js
@@ -1,6 +1,8 @@
-window.kievII = window.K2 = K2;
 if (typeof window.define === "function" && window.define.amd) {
   window.define("kievII", [], function() {
     return kievII;
   });
+}
+else {
+    window.kievII = window.K2 = K2;
 }
